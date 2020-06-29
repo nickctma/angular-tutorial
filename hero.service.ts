2a006f65bbad7123f5c9575b88567cfc0abbc09c
nickctma <67292244+nickctma@users.noreply.github.com>
2020-06-29 15:55:47 -0400

    Add files via upload

--- hero.service.ts
+++ hero.service.ts
@@ -3,6 +3,8 @@ import { Hero } from "./hero";
 import { HEROES} from "./mock-heroes";
 import { Observable, of} from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,38 @@ import { MessageService } from './message.service';
 export class HeroService {
 
   getHeroes(): Observable<Hero[]>{
-    return of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero>{
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';
+
+  private handleError<T>(operation = 'operation', result?: T){
+    return (error: any): Observable<T> => {
+      console.error(error):
+      this.log(`${operation} failed: ${error.message}`)
+      return of(result as T);
+    }
+  }
 }
--- heroes/heroes.component.ts
+++ heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from "../hero.service";
-import { throws } from 'assert';
 import { MessageService } from '../message.service';
 
 @Component({
@@ -14,17 +13,10 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void{
